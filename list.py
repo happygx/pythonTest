46bc37f997440b958410e26fbad9691f69700e28
happygx <463275576@qq.com>
2019-03-14 09:54:22 +0800

    tuple

--- list.py
+++ list.py
@@ -38,11 +38,17 @@ list = [1, 2, 3, 4, 5, 6, 7 ]
 # print(list)
 
 # 获取列表的第二个元素
-def takeSecond(elem):
-    return elem[1]
+# def takeSecond(elem):
+    # return elem[1]
 # 列表
-random = [(2, 2), (3, 4), (4, 1), (1, 3)]
+# random = [(2, 2), (3, 4), (4, 1), (1, 3)]
 # 指定第二个元素排序
-random.sort(key=takeSecond,reverse=True)
+# random.sort(key=takeSecond,reverse=True)
 # 输出类别
-print ('排序列表：', random)
+# print ('排序列表：', random)
+
+# list.clear()
+# print(list)
+
+# list2 = list.copy()
+# print(list2)
---
+++ tuple.py
@@ -0,0 +1,24 @@
+tup1 = (12, 34, 56)
+tup2 = ('abc', 'xyz')
+
+# print(tup1+tup2)
+# del tup1
+# print(tup1)
+
+# print(len(tup1))
+# print(tup1*3)
+# print(12 in tup1)
+# for x in tup1: print(x)
+
+# print(tup1[1])
+# print(tup1[-1])
+# print(tup1[1:2])
+# print(tup1[1:])
+# print(tup1[:1])
+
+# print(max(tup1))
+# print(min(tup1))
+
+# list= ['Google', 'Taobao', 'Runoob', 'Baidu']
+# tuple=tuple(list)
+# print(tuple)
