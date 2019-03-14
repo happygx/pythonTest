cb086d6c434518e6e0b58cfc87f9b90e41b769e4
happygx <463275576@qq.com>
2019-03-14 11:12:13 +0800

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,28 @@
+dict = {'Name': 'Runoob', 'Age': 7, 'Class': 'First'}
+
+# dict.clear()
+# print(dict)
+
+# dict1 = dict.copy()
+# print(dict1)
+
+# seq = ('name', 'age', 'sex')
+# dict2 = dict.fromkeys(seq, 10)
+# print (dict2)
+
+# print(dict.get('Age', 6))
+# print(dict.get('num', 6))
+# print('Age' in dict)
+# print(list(dict.keys()))
+# print(list(dict.values()))
+# dict.setdefault('num',6)
+# print(dict)
+
+# dict2 = {'Sex': 'female'}
+# dict.update(dict2)
+# print(dict)
+
+# print(dict.pop('Age'))
+# print(dict)
+print(dict.popitem())
+print(dict)
