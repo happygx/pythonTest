e7bf3e4d45a6d089091adc41bbe54d0c77b1aee1
happygx <463275576@qq.com>
2019-03-14 13:50:06 +0800

    set

---
+++ set.py
@@ -0,0 +1,41 @@
+set = set(("Google", "Runoob", "Taobao"))
+# set.add("Facebook")
+# print(set)
+
+# set.update({1,3})
+# print(set)
+# set.update([1,4],[5,6])
+# print(set)
+
+# set.remove("Taobao")
+# print(set)
+# set.remove("Taobao")
+# set.discard("Taobao")
+
+# set.pop()
+# print(set)
+# set.clear()
+# print(len(set))
+# print('Google' in set)
+
+# set1 = set.copy()
+# print(set1)
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+# print(x.difference(y))
+# x.difference_update(y)
+# print(x)
+
+# print(x.intersection(y))
+# x.intersection_update(y)
+# print(x)
+
+# print(x.isdisjoint(y))
+# print(x.symmetric_difference(y))
+# x.symmetric_difference_update(y)
+# print(x)
+# print(x.union(y))
+# x.update(y)
+# print(x)
